9152c3ec559beac211df24f34d2ea9c7615a5aea
dim-gggl <dimitri.gaggioli@gmail.com>
2025-06-19 12:21:04 +0200

    Update urls

---
+++ src/api_urls.py
@@ -0,0 +1,47 @@
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+
+from soft_desk_support.views import (
+    UserViewSet,
+    ProjectViewSet,
+    ContributorViewSet,
+    IssueViewSet,
+    CommentViewSet,
+)
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r"projects", ProjectViewSet)
+
+projects_router = routers.NestedSimpleRouter(
+    router, r"projects", lookup="project"
+)
+projects_router.register(
+    r"contributors",
+    ContributorViewSet,
+    basename="project_contributors"
+)
+projects_router.register(
+    r"issues", IssueViewSet, basename="project_issues"
+)
+
+issues_router = routers.NestedSimpleRouter(
+    projects_router, r"issues", lookup="issue"
+)
+issues_router.register(
+    r"comments", CommentViewSet, basename="issue_comments"
+)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(projects_router.urls)),
+    path('', include(issues_router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- src/soft_desk_support/urls.py
+++ src/soft_desk_support/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from .views import RootRedirectView
 
+import api_urls
+
 
 urlpatterns = [
     path("", RootRedirectView.as_view()),
     path('admin/', admin.site.urls),
-    path('api/', include('api_urls')),
+    path('api/', include(api_urls)),
 ]
