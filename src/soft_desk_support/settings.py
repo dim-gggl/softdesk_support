b23cd71ee687864a9bea886ba4a26f8442a20b3a
dim-gggl <dimitri.gaggioli@gmail.com>
2025-07-01 16:25:53 +0200

    Deletion of the STATIC_DIR parameter in settings

--- src/soft_desk_support/settings.py
+++ src/soft_desk_support/settings.py
@@ -129,8 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/" 
 
 AUTH_USER_MODEL = "user.User"
 
